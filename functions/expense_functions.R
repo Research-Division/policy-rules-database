2da7083e0354aadc7b55ec78c34e509c38e2ef4e
jtwalker-frba <jacob.walker@atl.frb.org>
2025-12-10 15:21:31 -0500

    Attempt to debug ALICE technology expenses

--- functions/expense_functions.R
+++ functions/expense_functions.R
@@ -258,6 +258,7 @@ function.transpExp.ALICE<-function(data){
 
 function.techExp.ALICE<-function(data){
 
+  exp.techData.ALICE<-test2
   # Add the most recent expenses to the current year if we do not have most up-to-date expenses
   years<-unique(data$Year) # years in data set
   yearsinexpdata<- unique(exp.techData.ALICE$yearofdata) # years in exp data
@@ -288,9 +289,9 @@ function.techExp.ALICE<-function(data){
       filter(yeardiff==minyeardiff) %>% select(-c(yeardiff, minyeardiff))
   }  # Attach copied future, historical, and missing expense data
   exp.techData.ALICE$Year<-exp.techData.ALICE$yearofdata
-  exp.techData.ALICE$stateAbbrev<-exp.techData.ALICE$State_Abbr
-  if(length(futureYrs)>0) { exp.techData.ALICE<-exp.techData.ALICE%>%rbind(expand) }
-  if(length(nonFutureYrs)>0) { exp.techData.ALICE<-exp.techData.ALICE%>%rbind(expandPastMiss2) } 
+  
+  if(length(futureYrs)>0) { exp.techData.ALICE<-exp.techData.ALICE%>%rbind(expand,fill=T) }
+  if(length(nonFutureYrs)>0) { exp.techData.ALICE<-exp.techData.ALICE%>%rbind(expandPastMiss2,fill=T) } 
   
   data<-left_join(data, exp.techData.ALICE, by=c("numadults", "Year","stateAbbrev"))
  
